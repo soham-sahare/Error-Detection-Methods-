2d3856f257e14a8c2948c66ed8ac7e92f3118198
Soham Sahare <44804981+sohamsahare123@users.noreply.github.com>
2020-03-30 19:49:37 +0530

    Update Error Detection CHECKSUM.py

--- Error Detection CHECKSUM.py
+++ Error Detection CHECKSUM.py
@@ -1,25 +1,38 @@
 data = []
 
-def reciever(s):
-    su = sum(data)
-    if (su == 0):
+def reciever(data, n):
+    s1 = 0
+    for c in range(0, n+1):
+        m1 = 0
+        m1 = data[c]
+        m1 = int(m1, 2)
+        s1 = s1 + m1
+    if (s1 == 0):
         print("Data Sent Successfully")
     else:
         print("Data is not sent Successfully")
 
-def sumsfunc(data):
-    s = sum(data)
-    print("Checksum : ",s)
-    data.append(-s)
+def sumsfunc(data, n):
+    s = 0
+    for c in range(0, n):
+        m = 0
+        m = data[c]
+        m = int(m,2)
+        s = s + m
+
+    print("Checksum : ",+s)
+    print("Checksum : ",bin(s))
+    data.append(bin(-s))
     print("SENDING.....")
     print("Transmitted data is : ",data)
-    reciever(data)
+
+    reciever(data, n)
 
 a = int(input("Enter the number of data bits to be transmitted : "))
 print("Enter the  bits to be transmitted ")
 
 for i in range (a):
     inp = int(input(">"))
-    data.append(inp)
+    data.append(bin(inp))
 
-sumsfunc(data)
+sumsfunc(data, a)
